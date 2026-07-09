4918e00a1315123fc9d835526368d8cb2f8117f7
NightbloomVoidstar <Jacob.cunningham12@outlook.com>
2026-07-08 21:16:58 -0400

    Fixed the bug where the return from the soulrend page plays the intro each time

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
 import IntroScreen from "./components/intro/IntroScreen";
 import Portfolio from "./components/Portfolio/Portfolio";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+
+  const [showIntro, setShowIntro] = useState(false);
   const [introComplete, setIntroComplete] = useState(false);
+
+  useEffect(() => {
+    const skipIntro =
+      searchParams.get("skipIntro") === "true";
+
+    if (skipIntro) {
+      setIntroComplete(true);
+    } else {
+      setShowIntro(true);
+    }
+  }, [searchParams]);
 
   return (
     <main className="relative min-h-screen bg-black">
 
-      {/* INTRO */}
-      {!introComplete && (
-        <div className="fixed inset-0 z-50">
-          <IntroScreen
-            onComplete={() => {
-              console.log("INTRO FINISHED");
-              setIntroComplete(true);
-            }}
-          />
-        </div>
-      )}
-
-
-      {/* PORTFOLIO */}
+      {/* Portfolio */}
       <div
-        className={`
+        className ={`
           transition-opacity duration-1000
           ${
             introComplete
@@ -36,6 +39,18 @@ export default function Home() {
       >
         <Portfolio />
       </div>
+
+      {/* Intro */}
+      {showIntro && (
+        <div className="fixed inset-0 z-50">
+          <IntroScreen
+            onComplete={() => {
+              setShowIntro(false);
+              setIntroComplete(true);
+            }}
+          />
+        </div>
+      )}
 
     </main>
   );
--- app/projects/soulrend/page.tsx
+++ app/projects/soulrend/page.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 export default function SoulrendPage() {
   return (
     <main className="min-h-screen bg-black text-white">
-      <Link href = "/" className ="fixed left-6 top-6 z-50 inline-flex items-center gap-2 rounded-md border border-white/10 bg-black/40 px-4 py-2 text-xs 
-      tracking-[0.3em] text-white/60 backdrop-blur-md transition hover:text-white hover:border-white/30 hover:bg-white/10"> ← RETURN</Link>
+      <Link href = "/?skipIntro=true" className ="fixed left-6 top-6 z-50 inline-flex items-center gap-2 rounded-md border border-white/10 bg-black/40 px-4 py-2 text-xs tracking-[0.3em] 
+      text-white/60 backdrop-blur-md transition hover:text-white hover:border-white/30 hover:bg-white/10"> ← RETURN</Link>
       
       {/* HERO */}
       <Hero/>
