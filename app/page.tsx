1e752c41bf298ede2d4e706b025e10aa6aa29ae9
NightbloomVoidstar <Jacob.cunningham12@outlook.com>
2026-07-08 20:59:53 -0400

    IntroScreen working again plus view projects scrolls and Resume button shows my resume

--- app/page.tsx
+++ app/page.tsx
@@ -5,36 +5,37 @@ import IntroScreen from "./components/intro/IntroScreen";
 import Portfolio from "./components/Portfolio/Portfolio";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const [introDone, setIntroDone] = useState(false);
+  const [introComplete, setIntroComplete] = useState(false);
 
   return (
-    <main className="relative min-h-screen">
+    <main className="relative min-h-screen bg-black">
 
-      {/* Portfolio */}
-      <div
-        className={`
-          transition-opacity duration-[2000ms]
-          ${introDone ? "opacity-100" : "opacity-0"}
-        `}
-      >
-        <Portfolio />
-      </div>
-
-      {/* Intro */}
-      {loading && (
+      {/* INTRO */}
+      {!introComplete && (
         <div className="fixed inset-0 z-50">
           <IntroScreen
             onComplete={() => {
-              setIntroDone(true);
-
-              setTimeout(() => {
-                setLoading(false);
-              }, 1800);
+              console.log("INTRO FINISHED");
+              setIntroComplete(true);
             }}
           />
         </div>
       )}
+
+
+      {/* PORTFOLIO */}
+      <div
+        className={`
+          transition-opacity duration-1000
+          ${
+            introComplete
+              ? "opacity-100"
+              : "opacity-0 pointer-events-none"
+          }
+        `}
+      >
+        <Portfolio />
+      </div>
 
     </main>
   );
